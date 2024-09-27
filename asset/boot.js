81843394779f86db84e560a6753776109a566f4d
Julian Erickson <julian@rothlabs.com>
2024-09-27 17:13:12 -0400

    fixed nurbs example

--- asset/boot.js
+++ asset/boot.js
@@ -1,11 +1,6 @@
 import init, * as app from 'init';
 async function run() {
-    console.log("here");
     await init({});
-    console.log("worked");
     // now call wasm methods on * as app as desired
 }
-setTimeout(() => {
-    run();
-}, 3000);
-  
+run();
