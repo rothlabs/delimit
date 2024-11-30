80353464925183171d2bbb5e9ee9712029ce408c
Julian Erickson <julian@rothlabs.com>
2024-11-30 16:13:48 -0500

    before store creates new bind groups when needed

--- edge/mech/src/core/bank.rs
+++ edge/mech/src/core/bank.rs
@@ -12,6 +12,7 @@ pub struct Bank {
 
 impl Bank {
     pub fn new(store: &Store, port: &Viewport) -> Result<Self> {
+        
         Ok(Self {
             plot: PlotBank::new(&port.gpu)?,
             draw: DrawBank::new(port)?,
